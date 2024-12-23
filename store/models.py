2216c487bde7454df6ebf2a9270141533922ac31
Kesara Hansajith <kesarahansaji@gmail.com>
2024-12-23 18:52:54 +0530

    Updated product cards

--- store/models.py
+++ store/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_level = models.IntegerField()
     discount = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)  
 
     def __str__(self):
         return self.name
@@ -18,3 +19,6 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order for {self.customer_name} - {self.product.name}"
+
+
+

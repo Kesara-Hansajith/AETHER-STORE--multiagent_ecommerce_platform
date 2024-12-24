29e9f2a850ba0fce6043a80ece7f4ad3e76efb66
GovinduKamburugamuwa <114232870+GovinduKamburugamuwa@users.noreply.github.com>
2024-12-24 09:35:35 +0530

    Feedback backend updated anc connected with ontology

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -128,5 +128,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = '/'
+
 
 
--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -19,3 +20,12 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order for {self.customer_name} - {self.product.name}"
+
+class Feedback(models.Model):
+    user = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback from {self.user} - Rating: {self.rating}"
